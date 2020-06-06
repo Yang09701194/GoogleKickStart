f66c87ba661f1fde5828f0b20239e6242b3b0067
Yang09701194 <mustnecessity@gmail.com>
2020-06-06 22:02:52 +0800

    20200606

---
+++ GoogleKickStart/GoogleKickStart/Problem/Countdown.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleKickStart.Problem
+{
+	class Countdown
+	{
+
+		public  static void Run()
+		{
+
+
+			List<int> nums = new List<int>();
+			List<string> info = new List<string>();
+
+			int caseCou = Convert.ToInt32(Console.ReadLine());
+
+
+
+			for (int i = 0; i < caseCou; i++)
+			{
+				int totalCountDown = 0;
+
+				info = Console.ReadLine().Split(' ').ToList();
+				int countdown = Convert.ToInt32(info[1]);
+				nums = Console.ReadLine().Split(' ').Select(s => Convert.ToInt32(s)).ToList();
+
+				int accumulateCuontdown = 0;
+				for (int j = 0; j <= nums.Count - countdown; j++)
+				{
+					if (nums[j] == countdown)
+					{
+						bool isCd = true;
+						int jump = 0;
+						for (int k = 0; k < countdown; k++)
+						{
+							if (nums[j + k] == countdown - k)
+							{
+								jump++;
+							}
+							else
+							{
+								isCd = false;
+								break;
+							}
+						}
+
+						if (isCd)
+						{
+							totalCountDown++;
+						}
+						j += jump;
+
+					}
+
+
+				}
+
+				Console.WriteLine($"Case #{i + 1}: {totalCountDown}");
+
+				
+			}
+
+
+
+		}
+
+	}
+}
---
+++ GoogleKickStart/GoogleKickStart/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoogleKickStart.Problem;
+
+namespace GoogleKickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Countdown.Run();
+
+
+
+			Console.Read(); 
+        }
+    }
+}
